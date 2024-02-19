30a494bcb8a4a15d4d013c54cc559da574339aa0
joshua chivile <joshuachivile@gmail.com>
2024-02-19 18:26:26 +0300

    React Router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
+import Home from "./pages/Home";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <>
-      <SignIn />
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<SignIn />}/>
+      <Route path="/register" element={<SignUp />}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
