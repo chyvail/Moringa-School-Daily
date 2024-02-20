f248f1885cc674380647e75676ba2b7ba5138a79
maureen <maureen.chelangat@student.moringaschool.com>
2024-02-20 06:46:33 +0300

    create a relationship between content and category

--- backend/models.py
+++ backend/models.py
@@ -44,6 +44,8 @@ class Category(db.Model, SerializerMixin):
     __tablename__ = 'categories'
     id =  db.Column(db.Integer, primary_key = True) 
     name = db.Column(db.String(50), unique = True)
+    contents = db.relationship("Content", backref = 'category', lazy=True)
+
 
     
 class Profile(db.Model,SerializerMixin):
