5351c65cde15054d1f463071e4e3c3a553412e4a
maureen <maureen.chelangat@student.moringaschool.com>
2024-02-20 06:31:40 +0300

    create contents model

--- backend/models.py
+++ backend/models.py
@@ -13,3 +13,17 @@ class User(db.Model,SerializerMixin):
     email = db.Column(db.String(50), unique= True)
     password = db.Column(db.String(50))
     role = db.Column(db.String(50))
+
+
+class Content(db.Model,SerializerMixin):
+    __tablename__ = 'contents'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50))
+    description = db.Column(db.String(350))
+    content_type = db.Column(db.String(50))
+    published_date = db.Column(db.DateTime, default=datetime.utcnow) 
+    content_url = db.Column(db.String(250))
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
+    flagged = db.Column(db.Boolean, default = False)
+    status = db.Column(db.Boolean, default = True)
