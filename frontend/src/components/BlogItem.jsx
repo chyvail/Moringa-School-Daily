7aa7f657b89ba8e56db808e5c074a91a1560628c
einstein <einsteinkibet0110@gmail.com>
2024-02-20 23:24:21 +0300

    edited the user page a little

--- frontend/src/components/BlogItem.jsx
+++ frontend/src/components/BlogItem.jsx
@@ -1,4 +1,4 @@
-export default function BlogItem({src,category,title,description}){
+export default function BlogItem({src,category,title,description,authorIcon,authorName}){
     return (
         <div className="card bg-light text-dark mb-3 d-inline-block my-3 mx-3 px-2 py-2 " style={{ width: '18rem' }}>
           <img src={src} style={{height:"200px", width:"360px"}} className="card-img-top" alt="..." />
