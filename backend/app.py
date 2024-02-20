68acf67266dc86380d9b0253bdf8235992289771
joshua chivile <joshuachivile@gmail.com>
2024-02-20 21:24:51 +0300

    Posting new user with hashing working

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 from http import HTTPStatus
+from controllers.users import Users
 from controllers.recommendation import Recommendations, RecommendationByID
 from controllers.subscription import Subscriptions, SubscriptionByID
 from controllers.wishlist import Wishlists, WishlistByID
@@ -35,12 +36,12 @@ class Home(Resource):
         return response
 
 api.add_resource(Home, '/')
-api.add_resource(Recommendations, '/recommendations')
 api.add_resource(RecommendationByID, '/recommendations<int:id>')
 api.add_resource(Subscriptions, '/subscriptions')
 api.add_resource(SubscriptionByID, '/subscriptions/<int:id>')
 api.add_resource(Wishlists, '/wishlists')
 api.add_resource(WishlistByID, '/wishlists/<int:id>')
+api.add_resource(Users,'/users')
 
 if __name__=='__main__':
     app.run()
---
+++ backend/controllers/users.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, make_response, jsonify
+from flask_restful import Api, Resource, abort
+from flask_bcrypt import Bcrypt
+from models import User,db
+
+bcrypt = Bcrypt()
+
+class Users(Resource):
+    def get(self):
+        users_dict = [users.to_dict() for users in User.query.all()]
+        return make_response(jsonify(users_dict), 200)
+    
+    def post(self):
+        data = request.get_json()
+        existing_user = User.query.filter_by(email=data['email']).first()
+        if existing_user:
+            abort(409, detail="User Already Exists")
+        if data['password'] == data['confirm-password']:
+            hashed_password = bcrypt.generate_password_hash(data['password'])
+            new_user = User(firstname=data['firstname'], lastname=data['lastname'], email=data['email'], password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return make_response(jsonify(new_user.to_dict()),201)
+    
+class UserLogin(Resource):
+    def post(self):
+        pass
