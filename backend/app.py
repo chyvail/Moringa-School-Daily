ad2c572af181774feed868ada33fb1ce6f905baf
joshua chivile <joshuachivile@gmail.com>
2024-02-20 20:09:30 +0300

    Added Flask Migrate, CORS and fixed spelling on classes

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,23 @@ from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 from http import HTTPStatus
 from controllers.recommendation import Recommendations, RecommendationByID
-from controllers.subscription import Subscription, SubscriptionByID
-from controllers.wishlist import Wishlist, WishlistByID
+from controllers.subscription import Subscriptions, SubscriptionByID
+from controllers.wishlist import Wishlists, WishlistByID
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+from models import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///moringa-daily.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app,db)
+
+db.init_app(app)
+
+CORS(app)
 
 api = Api(app)
 
@@ -24,9 +37,9 @@ class Home(Resource):
 api.add_resource(Home, '/')
 api.add_resource(Recommendations, '/recommendations')
 api.add_resource(RecommendationByID, '/recommendations<int:id>')
-api.add_resource(Subscription, '/subscriptions')
+api.add_resource(Subscriptions, '/subscriptions')
 api.add_resource(SubscriptionByID, '/subscriptions/<int:id>')
-api.add_resource(Wishlist, '/wishlists')
+api.add_resource(Wishlists, '/wishlists')
 api.add_resource(WishlistByID, '/wishlists/<int:id>')
 
 if __name__=='__main__':
