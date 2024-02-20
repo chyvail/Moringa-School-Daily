5a71abd0a785a0969dbb1f837264efc392d5b541
maureen <maureen.chelangat@student.moringaschool.com>
2024-02-20 17:54:11 +0300

    create recommendations resource

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,27 @@
 from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 from http import HTTPStatus
+from controllers.recommendation import Recommendation, RecommendationByID
 
 app = Flask(__name__)
 
 api = Api(app)
 
-# class Home(Resource):
+class Home(Resource):
     
-#     def get(self):
-#         response_dict = {
-#             "Message": "Moringa School Daily API",
-#         }
-#         response = make_response(
-#             response_dict,
-#             HTTPStatus.OK,
-#         )
-#         return response
+    def get(self):
+        response_dict = {
+            "Message": "Moringa School Daily API",
+        }
+        response = make_response(
+            response_dict,
+            HTTPStatus.OK,
+        )
+        return response
 
-# api.add_resource(Home, '/')
+api.add_resource(Home, '/')
+api.add_resource(Recommendation, '/recommendations')
+api.add_resource(RecommendationByID, '/recommendations<int:id>')
 
 if __name__=='__main__':
     app.run()
