393d8a43ee449033c24df6ca22dd84796ff81398
joshua chivile <joshuachivile@gmail.com>
2024-02-21 10:48:52 +0300

    Hero section complete

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <>
+      <div className="container-lgs mt-4 hero-top">
+        <p>
+          Home . <span> Blogs and New Content</span>
+        </p>
+        <h3>Hello Sabrina, Welcome to Moringa Daily</h3>
+        <p className="hero-description">
+          Access authentic and verified information, inspiration, and advice
+          about the tech space.
+        </p>
+      </div>
+      <hr />
+    </>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Nav from '../components/Nav'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div>HomePage</div>
+    <>
+      <Nav />
+      <Hero />
+    </>
   )
 }
