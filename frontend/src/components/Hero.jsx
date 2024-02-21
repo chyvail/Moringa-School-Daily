e2d42c6c4ff29e3e7b0fd7338b8a9e3aa0a1b2d0
joshua chivile <joshuachivile@gmail.com>
2024-02-21 12:35:12 +0300

    Dynamically render user

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { SchoolContext } from "../contexts/SchoolContext";
 
 export default function Hero() {
+  const { user } = useContext(SchoolContext);
   return (
     <>
       <div className="container-lgs mt-4 hero-top">
         <p>
           Home . <span> Blogs and New Content</span>
         </p>
-        <h3>Hello Sabrina, Welcome to Moringa Daily</h3>
+        <h3>Hello {user || "Guest User"}, Welcome to Moringa Daily</h3>
         <p className="hero-description">
           Access authentic and verified information, inspiration, and advice
           about the tech space.
